05b0f7db493f88a0647919744bbaf6b919805a26
user <user@DESKTOP-AM72P98>
2022-08-24 17:35:23 +0530

    12345697

--- src/test/java/sampleframeworkpractice/Sampletest.java
+++ src/test/java/sampleframeworkpractice/Sampletest.java
@@ -4,5 +4,6 @@ public class Sampletest {
 public static void main(String[] args) {
 	System.out.println("testcase1 created");
 	System.out.println("testcase2 created");
+	System.out.println("testcase3 created");
 }
 }
