4aafa2b870a4ff2d385214383117f17ee473a54a
User <User@DESKTOP-P6508QM>
2022-08-24 11:30:06 +0530

    hello

---
+++ src/test/java/sampleframeworkpractice/Sampletest.java
@@ -0,0 +1,8 @@
+package sampleframeworkpractice;
+
+public class Sampletest {
+public static void main(String[] args) {
+	System.out.println("testcase1 created");
+	System.out.println("testcase2 created");
+}
+}
