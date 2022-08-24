adc8721b3ddd1aedfbb575a7e0981aa1e22a7d03
User <User@DESKTOP-P6508QM>
2022-08-24 17:34:41 +0530

    branch1

--- src/test/java/sampleframeworkpractice/fg.java
+++ src/test/java/sampleframeworkpractice/fg.java
@@ -2,7 +2,11 @@ package sampleframeworkpractice;
 
 public class fg {
 	public void voidd() {
-		System.out.println("asdfghjkl;");
+		System.out.println("asdfghjkl");
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
 }
 
 }
